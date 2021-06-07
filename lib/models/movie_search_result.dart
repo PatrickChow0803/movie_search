aef5be8f62a357185444d808cdc7701c22b7bdbe
Patrick Chow <patrickchow0803@gmail.com>
2021-06-07 08:14:42 -0500

    added movie search result model

---
+++ lib/models/movie_search_result.dart
@@ -0,0 +1,14 @@
+class MovieSearchResult {
+  final String title;
+  final String year;
+  final String imdbID;
+  final String type;
+  final String posterUrl;
+  MovieSearchResult({
+    required this.title,
+    required this.year,
+    required this.imdbID,
+    required this.type,
+    required this.posterUrl,
+  });
+}
